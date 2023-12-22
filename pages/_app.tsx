761f9e49e71accebf7d6085725e2eabbd9b0b254
Alfredo Frangoulis <alfredofrangoulis@gmail.com>
2023-12-22 12:14:40 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageprops },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageprops} />
+    </SessionProvider>
+  );
+}
